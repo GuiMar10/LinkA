1ca3ba1c32c635e3a62f6ac5151f997255ccd565
guilherme.marmiroli <guilherme.marmiroli@gmail.com>
2024-10-29 18:41:30 +0000

    Alpha Release

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,44 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import svelteLogo from "./assets/svelte.svg";
+  import viteLogo from "/vite.svg";
+  import Counter from "./lib/Counter.svelte";
 </script>
 
 <h2>Oie, papai!</h2>
-<input type="text" placeholder="Pesquisar">
+<div id="search">
+  <input type="text" placeholder="Pesquisar" />
+  <button>b</button>
+</div>
 <div class="card">Card ;)</div>
 <div class="card">Card ;)</div>
 
-<style>
+<style lang="scss">
+  :global(body){
+    font-family: More Sugar;
+    --borderColor: #521887;
+  }
   @font-face {
     font-family: More Sugar;
-    src: url('./assets/MoreSugar-Regular.otf')
+    src: url("./assets/MoreSugar-Regular.otf");
+  }
+  #search {
+    width: max-content;
+    border-radius: 20px;
+    transition: 0.2s ease;
+    &:active {
+      scale: 0.95 0.9;
+    }
+    & button {
+      position: relative;
+      border-radius: 50px;
+      background: #9e6df7;
+      color: white;
+      border: 1px solid var(--borderColor);
+      cursor: pointer;
+      height: 39px;
+      width: 39px;
+      transform: translateX(-52px) translateY(-2px);
+    }
   }
   input {
     border-radius: 100px;
@@ -22,13 +48,11 @@
     border-bottom: 7px solid;
     border-left: 4px solid;
     border-right: 4px solid;
-    transition: 0.2s ease;
     border-color: #882fda;
-    outline: #521887 solid 2px;
+    outline: var(--borderColor) solid 2px;
     outline-offset: -1px;
-  }
-  input:active {
-    scale: 0.95 0.9;
+    width: 180px;
+    font-family: More Sugar;
   }
   .card {
     border-radius: 10px;
@@ -36,14 +60,19 @@
     margin-top: 12px;
     transition: 0.2s;
     cursor: pointer;
-    border-bottom: 3px solid;
-    border-right: 3px solid;
-  }
-  .card:hover{
+    box-shadow: 2px 2px 2px 2px rgb(175, 143, 230);
+    user-select: none;
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    place-content: center;
+    &:hover {
     scale: 1.02;
   }
-  .card:active {
+  &:active {
     scale: 1;
+    box-shadow: none;
+  }
   }
   h2 {
     font-family: More Sugar;
